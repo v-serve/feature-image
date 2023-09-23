062e59ceb52347baf61163e01ca15c162d6f8044
v-serve <123567597+v-serve@users.noreply.github.com>
2023-09-24 02:21:19 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import streamlit as st
 from PIL import Image
-import io
 
-# Function to add watermark to the background image
-def add_watermark(background_path, watermark_path):
-    background = Image.open(background_path).convert("RGBA")
-    watermark = Image.open(watermark_path).convert("RGBA")
+# Function to apply watermark to the uploaded image
+def apply_watermark(background_image):
+    watermark = Image.open("blue-watermark.png").convert("RGBA")
 
-    # Get the current width and height of the background image
-    width, height = background.size
+    # Get the current width and height of the image
+    width, height = background_image.size
     watermark_width, watermark_height = watermark.size
 
     back = Image.new(mode="RGBA", size=(width, height + 40), color=(255, 255, 255))
-    back.paste(background, (0, 0), background)
+    back.paste(background_image, (0, 0), background_image)
 
     # Check if the width is greater than 1800 pixels
     if width > 1800:
@@ -28,46 +26,38 @@ def add_watermark(background_path, watermark_path):
         position = (0, new_height - watermark_height + 40)
 
     # Calculate the position to paste the watermark at the bottom
-    watermark_cropped = watermark.crop(
-        (
-            (watermark_width - new_width) / 2,
-            0,
-            (watermark_width - new_width) / 2 + new_width + 1,
-            new_height,
-        )
-    )
+    watermark_cropped = watermark.crop(((watermark_width - new_width) / 2, 0,
+                                        (watermark_width - new_width) / 2 + new_width + 1, new_height))
 
-    # Paste the watermark onto the background image
+    # Paste the watermark onto the back image
     back.paste(watermark_cropped, position, watermark_cropped)
 
-    # Convert the result to RGB format
+    # Save the result as a PNG
     back = back.convert("RGB")
-
     return back
 
-# Streamlit app
+# Streamlit UI
 st.title("Watermark App")
+st.write("Upload a background image or paste an image link below:")
+uploaded_image = st.file_uploader("Upload Image", type=["jpg", "jpeg", "png"])
+image_url = st.text_input("Or paste an image URL")
 
-# Upload background image
-background_image = st.file_uploader("Upload a background image", type=["jpg", "jpeg", "png"])
+if uploaded_image is not None:
+    background_image = Image.open(uploaded_image).convert("RGBA")
+    result = apply_watermark(background_image)
+    st.image(result, use_column_width=True, caption="Watermarked Image")
+    st.markdown("### Download Watermarked Image")
+    st.write("Click the link below to download the watermarked image:")
+    st.download_button("Download Watermarked Image", result.tobytes(), file_name="watermarked_image.png", key="watermarked")
 
-if background_image is not None:
-    watermark_path = "blue-watermark.png"  # Path to the fixed watermark
-
-    # Apply watermark to the background image
-    watermarked_image = add_watermark(background_image, watermark_path)
-
-    # Save the watermarked image to a temporary file
-    temp_buffer = io.BytesIO()
-    watermarked_image.save(temp_buffer, format="PNG")
-
-    # Display the watermarked image
-    st.image(temp_buffer, caption="Watermarked Image", use_column_width=True)
-
-    # Provide a download link for the watermarked image
-    st.download_button(
-        label="Download Watermarked Image",
-        data=temp_buffer.getvalue(),
-        file_name="watermarked_image.png",
-        key="download-button",
-    )
+if image_url:
+    try:
+        from urllib.request import urlopen
+        background_image = Image.open(urlopen(image_url)).convert("RGBA")
+        result = apply_watermark(background_image)
+        st.image(result, use_column_width=True, caption="Watermarked Image")
+        st.markdown("### Download Watermarked Image")
+        st.write("Click the link below to download the watermarked image:")
+        st.download_button("Download Watermarked Image", result.tobytes(), file_name="watermarked_image.png", key="watermarked")
+    except Exception as e:
+        st.error("Error: Unable to load the image from the provided URL.")
