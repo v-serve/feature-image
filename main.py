6596614d89f3c0eea2e52960553ab4a2c4a1cd53
v-serve <123567597+v-serve@users.noreply.github.com>
2023-09-24 00:49:32 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,121 +1,50 @@
 import streamlit as st
 from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
-import io
 
-# Function to apply code 1 to the image
-def apply_code_1(icon_img, text):
-    back = Image.open("frame.png").convert("RGBA")
+# Function to add watermark to the image
+def add_watermark(background_image):
+    backgound = Image.open(background_image).convert("RGBA")
+    watermark = Image.open("blue-watermark.png").convert("RGBA")
 
-    # Create a copy of the icon to work with
-    whited_icon = icon_img.copy()
+    # Get the current width and height of the image
+    width, height = backgound.size
+    watermark_width, watermark_height = watermark.size
 
-    # Iterate through the pixels of the icon and change non-transparent pixels to white
-    for x in range(icon_img.width):
-        for y in range(icon_img.height):
-            r, g, b, a = whited_icon.getpixel((x, y))
-            if a > 0:
-                whited_icon.putpixel((x, y), (255, 255, 255, a))
+    back = Image.new(mode="RGBA", size=(width, height+40), color=(255, 255, 255))
+    back.paste(backgound, (0,0), backgound)
 
-    # Resize the modified icon
-    whited_icon = whited_icon.resize((250, 250))
+    # Check if the width is greater than 1800 pixels
+    if width > 1800:
+        # Calculate the new height while maintaining the aspect ratio
+        new_width = 1800
+        new_height = int(height * (1800 / width))
+        position = (0, new_height - watermark_height)
+        # Resize the image to a maximum width of 1800 pixels
+        back = back.resize((new_width, new_height), Image.ANTIALIAS)
+    else:
+        new_width = width
+        new_height = height
+        position = (0, new_height - watermark_height + 40)
 
-    # Calculate the X-coordinate to center the icon horizontally
-    x_centered = (back.width - whited_icon.width) // 2
+    # Calculate the position to paste the watermark at the bottom
+    watermark_cropped = watermark.crop(((watermark_width-new_width)/2, 0,(watermark_width-new_width)/2 + new_width+1,new_height))
+    
+    # Paste the watermark onto the back image
+    back.paste(watermark_cropped, position, watermark_cropped)
 
-    # Paste the modified icon onto the background at the calculated X-coordinate
-    back.paste(whited_icon, (x_centered, 100), whited_icon)
-
-    font = ImageFont.truetype("IRYekan.ttf", size=60, layout_engine=ImageFont.LAYOUT_RAQM)
-    I1 = ImageDraw.Draw(back)
-
-    lines = []
-    current_line = ""
-    for word in text.split():
-        if I1.textsize(current_line + word + " ", font=font)[0] > 900:
-            lines.append(current_line.strip())
-            current_line = ""
-        current_line += word + " "
-    if current_line:
-        lines.append(current_line.strip())
-
-    y = 375
-    for line in lines:
-        _, _, w, h = I1.textbbox((0, 0), line, font=font)
-        x = (back.width - w) / 2
-        I1.text((x, y - 15), line, fill=(255, 255, 255), font=font, align="center", direction="rtl")
-        y += h
-
+    # Save the result
+    back = back.convert("RGB")
     return back
 
-# Function to apply code 2 to the image
-def apply_code_2(icon_img, text):
-    back = Image.open("white-frame.png").convert("RGBA")
+# Streamlit app
+st.title("Arzchi Watermarking App")
 
-    # Create a copy of the icon to work with
-    whited_icon = icon_img.copy()
+# Upload background image
+background_image = st.file_uploader("Upload background image", type=["jpg", "jpeg", "png"])
+if background_image:
+    #st.write("Original Image:")
+    #st.image(background_image, use_column_width=True)
 
-    # Iterate through the pixels of the icon and change non-transparent pixels to a dark color
-    for x in range(icon_img.width):
-        for y in range(icon_img.height):
-            r, g, b, a = whited_icon.getpixel((x, y))
-            if a > 0:
-                whited_icon.putpixel((x, y), (25, 52, 152, a))
-
-    # Resize the modified icon
-    whited_icon = whited_icon.resize((250, 250))
-
-    # Calculate the X-coordinate to center the icon horizontally
-    x_centered = (back.width - whited_icon.width) // 2
-
-    # Paste the modified icon onto the background at the calculated X-coordinate
-    back.paste(whited_icon, (x_centered, 100), whited_icon)
-
-    font = ImageFont.truetype("IRYekan.ttf", size=60, layout_engine=ImageFont.LAYOUT_RAQM)
-    I1 = ImageDraw.Draw(back)
-
-    lines = []
-    current_line = ""
-    for word in text.split():
-        if I1.textsize(current_line + word + " ", font=font)[0] > 900:
-            lines.append(current_line.strip())
-            current_line = ""
-        current_line += word + " "
-    if current_line:
-        lines.append(current_line.strip())
-
-    y = 375
-    for line in lines:
-        _, _, w, h = I1.textbbox((0, 0), line, font=font)
-        x = (back.width - w) / 2
-        I1.text((x, y - 15), line, fill=(25, 52, 152), font=font, align="center", direction="rtl")
-        y += h
-
-    return back
-
-# Main Streamlit app
-def main():
-    st.title("Image Generator")
-
-    # Upload icon image
-    icon_image = st.file_uploader("Upload Icon Image", type=["jpg", "png", "jpeg"])
-    if icon_image:
-        icon_img = Image.open(icon_image).convert("RGBA")
-
-        # Input text
-        text = st.text_input("Enter Text", "معاملات اسپات چیست؟")
-
-        # Background style selection
-        background_style = st.selectbox("Select Background Style", ["White", "Dark"])
-
-        if st.button("Generate Image"):
-            if background_style == "White":
-                generated_image = apply_code_1(icon_img, text)
-            else:
-                generated_image = apply_code_2(icon_img, text)
-
-            st.image(generated_image, use_column_width=True)
-
-if __name__ == "__main__":
-    main()
+    st.write("Watermarked Image:")
+    watermarked_image = add_watermark(background_image)
+    st.image(watermarked_image, use_column_width=True)
